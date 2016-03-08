cc3e3e2f618bd6859e81d42bdbd9280aa4c9ac9d
Elvin Rakhmankulov <elvin_rakhmankulov@epam.com>
2016-03-08 13:57:31 -0600

    Adding empty xcode project

---
+++ ColoredDiceGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColoredDiceGame
+//
+//  Created by Elvin Rakhmankulov on 3/8/16.
+//  Copyright © 2016 Buffalo Grove Objective-C and Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
